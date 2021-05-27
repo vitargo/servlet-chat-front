57bdb0b9c88960193e4f3ed1614db4d1cea2d489
Vita Redko <rvitargo@gmail.com>
2021-05-27 09:37:11 +0300

    add working post request and auth page

--- src/index.js
+++ src/index.js
@@ -1,84 +1,164 @@
-import _ from 'lodash';
-import './style.css';
-import Icon from './bg.jpeg';
+import './style.css'
+import Icon from 'img/logo.png';
+import axios from "axios";
 
-var socket;
-var currentUser;
-var Token
 
-function component() {
-    const element = document.createElement('div');
+let socket;
+let currentUser;
+let Token;
 
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
 
-    // Add the image to our existing div.
-    const myIcon = new Image();
-    myIcon.src = Icon;
+const root = document.getElementById('root');
 
-    element.appendChild(myIcon);
+let div_logo_container = document.createElement('div');
+div_logo_container.setAttribute('name', 'logo_container');
+div_logo_container.setAttribute('class', 'logo_container');
 
-    return element;
-}
+let logo_place = document.createElement('div');
+logo_place.setAttribute('name', 'logo');
+logo_place.setAttribute('class', 'logo');
 
-document.body.appendChild(component());
-// эта функция сработает при нажатии на кнопку
+const myIcon = new Image();
+myIcon.src = Icon;
+logo_place.appendChild(myIcon);
+
+let name_place = document.createElement('span');
+
+let name_h1 = document.createElement('h1');
+let name_text = document.createTextNode('PinkLink Chat');
+name_h1.appendChild(name_text);
+name_place.appendChild(name_h1);
+
+let div_form_container = document.createElement('div');
+div_form_container.setAttribute('name', 'form_container');
+div_form_container.setAttribute('class', 'form_container');
+
+let br = document.createElement('br');
+
+let form = document.createElement('form');
+form.setAttribute('name', 'AuthForm')
+form.setAttribute('class', 'form');
+form.setAttribute('method', 'post');
+form.setAttribute('autocomplete', 'off');
+// form.setAttribute('onsubmit', 'signIn()');
+
+let login = document.createElement('input');
+login.setAttribute('type', 'text');
+login.setAttribute('class', 'form__input');
+login.setAttribute('name', 'login');
+login.setAttribute('placeholder', 'Enter login or email');
+login.setAttribute('maxlength', '10');
+login.setAttribute('required', '');
+
+let error_login = document.createElement('div');
+error_login.setAttribute('class', 'error');
+error_login.setAttribute('name', 'error_login');
+
+// let span_sp = document.createElement('span');
+// span_sp.setAttribute('class', 'inline');
+
+let PSW = document.createElement('input');
+PSW.setAttribute('type', 'password');
+PSW.setAttribute('class', 'form__input');
+PSW.setAttribute('name', 'password');
+PSW.setAttribute('placeholder', 'Password');
+PSW.setAttribute('maxlength', '10');
+PSW.setAttribute('required', '');
+PSW.setAttribute('style', 'margin-left: 0px');
+
+// let checkbox = document.createElement('input');
+// checkbox.setAttribute('id', 'checkbox');
+// checkbox.setAttribute('type', 'checkbox');
+// checkbox.setAttribute('onclick', 'showPassword()');
+//
+// let label = document.createElement('label');
+// label.setAttribute('class', 'checkbox__label');
+// label.setAttribute('for','checkbox');
+// label.appendChild(document.createTextNode('Show password'));
+
+
+let error_PSW = document.createElement('div');
+error_PSW.setAttribute('class', 'error');
+error_PSW.setAttribute('name', 'error_password');
+
+let btn = document.createElement('button');
+btn.setAttribute('type', 'submit');
+btn.setAttribute('class', 'form__btn');
+btn.textContent = 'SIGN IN';
+
+root.appendChild(div_logo_container);
+root.appendChild(div_form_container);
+div_logo_container.appendChild(logo_place);
+div_logo_container.appendChild(name_place);
+form.appendChild(login);
+form.appendChild(error_login);
+form.appendChild(br.cloneNode());
+//form.appendChild(span_sp);
+// span_sp.appendChild(PSW);
+// span_sp.appendChild(checkbox);
+// span_sp.appendChild(label);
+form.appendChild(PSW);
+form.appendChild(error_PSW);
+form.appendChild(br.cloneNode());
+form.appendChild(btn);
+
+div_form_container.appendChild(form);
+
+// let log = document.forms['AuthForm']['login'];
+// let pass = document.forms['AuthForm']['password'];
+
+//login.addEventListener('input', validateLogin);
+// PSW.addEventListener('input', validatePSW);
+btn.addEventListener('click', signIn);
+
 function signIn() {
-    console.log("zashlo");
-    let login = document.querySelector('#login');
-    let password = document.querySelector('#password');
-    let result = document.querySelector('.result');
+    let login = document.forms['AuthForm']['login'].value;
+    let password = document.forms['AuthForm']['password'].value;
     let xhr = new XMLHttpRequest();
-    let url = window.location.href;
-    //url = window.location.replace("http://localhost:8081/chat/auth");
-    if (loginValidator(login.value, password.value)){
-        xhr.open("POST", url, true);
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.onreadystatechange = function (){
-            if (xhr.readyState === XMLHttpRequest.DONE) {
-                switch (xhr.status) {
-                    case 200:
-                        currentUser = login;
-                        var webSocketAccessToken = xhr.responseText;
-                        Token = webSocketAccessToken.token;
-                        console.log(webSocketAccessToken.token);
-                        openSocket(webSocketAccessToken.token);
-                        break;
-                    case 403:
-                        console.log("Lox");
-                        break;
-                    default:
-                        document.getElementById("authentication-error").innerHTML = "Oops... Looks like something is broken.";
-                }
+    let url = "http://localhost:8082/chat/auth";
+    // window.location.href;
+    // url = window.location.replace("http://localhost:8082/chat/auth");
+    xhr.open("POST", url);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            switch (xhr.status) {
+                case 200:
+                    currentUser = login;
+                    var webSocketAccessToken = xhr.responseText;
+                    Token = webSocketAccessToken.token;
+                    console.log(webSocketAccessToken.token);
+                    openSocket(webSocketAccessToken.token);
+                    break;
+                case 403:
+                    currentUser = null;
+                    document.getElementById("error_login").innerHTML = "Oops... These credentials are invalid.";
+                    break;
+                default:
+                    document.getElementById("error_login").innerHTML = "Oops... Looks like something is broken.";
             }
-        };
+        }
+    };
 
-        var data = JSON.stringify({ "login": login.value, "password": password.value });
-        xhr.send(data);
-        console.log(xhr.readyState);
-    }
+    let data = JSON.stringify({"login": login, "password": password});
+    xhr.send(data);
+    //
+    // let s = axios({
+    //     method: 'post',
+    //     url: "http://localhost:8082/chat/auth",
+    //     data: {
+    //         login: login,
+    //         password: password,
+    //     }
+    // })
+    //     .then((response) => {
+    //         console.log(response);
+    //     }, (error) => {
+    //         console.log(error);
+    //     });
+
 }
 
-function loginValidator(login, pass){
-    if (login === "") {
-        document.getElementById("login").style.border = "1px solid red";
-        return false;
-    } else if (pass === "" || pass.length < 4) {
-        document.getElementById("password").style.border = "1px solid red";
-        return false;
-    } else {
-        console.log(login,pass);
-        return true;
-    }
-
-}
-
-
-function connectTo(){
-    console.log(JSON.stringify({"topic":"auth","payload":"" + Token }));
-    socket.send(JSON.stringify({"topic":"auth","payload":"" + Token }));
-}
 function openSocket(accessToken) {
     let login = document.querySelector('#login');
     let password = document.querySelector('#password');
@@ -95,32 +175,21 @@ function openSocket(accessToken) {
     console.log(data);
     currentUser = login.value;
     xhrs.send(data);
-    xhrs.onerror = function (){
-        console.log("ошибочка произошшла");
-    }
-    console.log("responseXML "+xhrs.responseXML);
-    console.log("resp "+xhrs.response);
+    console.log("responseXML"+xhrs.responseXML);
+    console.log("resp  "+xhrs.response.token);
     console.log("openSocket");
 
     if (socket) {
         socket.close();
     }
-
-
     socket = new WebSocket("ws://localhost:8081/chat");
     xhrs.onload = function () {
-        if (xhrs.status === 200 ){
-            var au = JSON.stringify({"topic":"auth","payload":xhrs.response});
-            console.log("au ksta" + au);
-            socket.send(au);
-        }
-        else {
-            alert("неправильный логин и/или пароль");
-        }
+        var au = JSON.stringify({"topic":"auth","payload":xhrs.response});
+        console.log(au);
+        socket.send(au);
     }
 
     socket.onopen = function (event) {
-        console.log("Да блять а тут я то чо делаю.")
         document.getElementById("authentication").style.display = "none";
         document.getElementById("contacts").style.display = "block";
         document.getElementById("chat").style.display = "block";
@@ -131,116 +200,3 @@ function openSocket(accessToken) {
     socket.onmessage = onMessage;
 }
 
-var onMessage = function(event) {
-    var webSocketMessage = JSON.parse(event.data);
-    switch (webSocketMessage.topic) {
-        case "auth":
-            displayConnectedUserMessage(currentUser);
-            break;
-        case "sendTextMessage":
-            displayMessage(currentUser,webSocketMessage.payload);
-            break;
-    }
-};
-
-function sendMessage() {
-
-    var text = document.getElementById("message").value;
-    document.getElementById("message").value = "";
-
-    var payload = text;
-
-    var webSocketMessage = {
-        topic: "sendTextMessage"
-    };
-
-    webSocketMessage.payload = payload;
-
-    socket.send(JSON.stringify(webSocketMessage));
-    socket.onmessage = onMessage;
-}
-
-function displayMessage(username, text) {
-
-    var sentByCurrentUer = currentUser === username;
-
-    var message = document.createElement("div");
-    message.setAttribute("class", sentByCurrentUer === true ? "message sent" : "message received");
-    message.dataset.sender = username;
-
-    var sender = document.createElement("span");
-    sender.setAttribute("class", "sender");
-    sender.appendChild(document.createTextNode(sentByCurrentUer === true ? "You" : username));
-    message.appendChild(sender);
-
-    var content = document.createElement("span");
-    content.setAttribute("class", "content");
-    content.appendChild(document.createTextNode(text));
-    message.appendChild(content);
-
-    var messages = document.getElementById("messages");
-    var lastMessage = messages.lastChild;
-    if (lastMessage && lastMessage.dataset.sender && lastMessage.dataset.sender === username) {
-        message.className += " same-sender-previous-message";
-    }
-
-    messages.appendChild(message);
-    messages.scrollTop = messages.scrollHeight;
-}
-
-function displayConnectedUserMessage(username) {
-
-    var sentByCurrentUer = currentUser === username;
-
-    var message = document.createElement("div");
-    message.setAttribute("class", "message event");
-
-    var text = sentByCurrentUer === true ? "Welcome " + username : username + " joined the chat";
-    var content = document.createElement("span");
-    content.setAttribute("class", "content");
-    content.appendChild(document.createTextNode(text));
-    message.appendChild(content);
-
-    var messages = document.getElementById("messages");
-    messages.appendChild(message);
-}
-
-function displayDisconnectedUserMessage(username) {
-
-    var message = document.createElement("div");
-    message.setAttribute("class", "message event");
-
-    var text = username + " left the chat";
-    var content = document.createElement("span");
-    content.setAttribute("class", "content");
-    content.appendChild(document.createTextNode(text));
-    message.appendChild(content);
-
-    var messages = document.getElementById("messages");
-    messages.appendChild(message);
-}
-
-function addAvailableUsers(username) {
-
-    var contact = document.createElement("div");
-    contact.setAttribute("class", "contact");
-
-    var status = document.createElement("div");
-    status.setAttribute("class", "status");
-    contact.appendChild(status);
-
-    var content = document.createElement("span");
-    content.setAttribute("class", "name");
-    content.appendChild(document.createTextNode(username));
-    contact.appendChild(content);
-
-    var contacts = document.getElementById("contacts");
-    contacts.appendChild(contact);
-}
-
-function cleanAvailableUsers() {
-    var contacts = document.getElementById("contacts");
-    while (contacts.hasChildNodes()) {
-        contacts.removeChild(contacts.lastChild);
-    }
-}
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,100 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const DirectoryNamedWebpackPlugin = require("directory-named-webpack-plugin");
+
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        main: path.resolve(__dirname, './src/index.js'),
+    },
     mode: 'development',
     devtool: 'inline-source-map',
     devServer: {
-        contentBase: './dist',
+        historyApiFallback: true,
+        contentBase: path.resolve(__dirname, './dist'),
+        open: true,
+        compress: true,
+        hot: true,
+        port: 8080,
     },
     plugins: [
+        new MiniCssExtractPlugin(
+    //         {
+    //     filename: "[name].css"
+    // }
+    ),
         new HtmlWebpackPlugin({
             filename: 'index.html',
-            template: 'dist/index.html'
+            template: path.resolve(__dirname, './src/auth.html'),
         }),
-        // new HtmlWebpackPlugin({
-        //     filename: 'example.html',
-        //     template: 'dist/registration.html',
-        //     chunks: ['regEntry']
-        // })
+        // new CleanWebpackPlugin(),
     ],
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-        publicPath: '/',
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js',
     },
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
             },
+            // {
+            //     test: /\.css$/,
+            //     use: [
+            //         'style-loader',
+            //         {
+            //             loader: 'css-loader',
+            //             options: {
+            //                 importLoaders: 1,
+            //                 modules: true
+            //             }
+            //         }
+            //     ],
+            //     // include: /\.module\.css$/
+            // },
+            // { test: /\.txt$/, use: 'raw-loader' },
+
+
+            // {
+            //     test: /\.css$/,
+            //     use: [
+            //         'style-loader',
+            //         'css-loader',
+            //     ]
+            // },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },        ],
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+            // {
+            //     test: /\.js$/,
+            //     exclude: /node_modules/,
+            //     use: ['babel-loader'],
+            // },
+        ],
+    },
+    resolveLoader: {
+        modules: [
+            path.join(__dirname, 'node_modules')
+        ]
+    },
+    resolve: {
+        descriptionFiles: ['package.json'],
+        enforceExtension: false,
+        aliasFields: ['browser'],
+        mainFiles: ['index'],
+        modules: [
+            path.join(__dirname, 'node_modules')
+        ],
+        plugins: [new DirectoryNamedWebpackPlugin()],
+        preferRelative: true,
 
     },
 };
